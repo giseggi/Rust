ee8c16ea1f4c3d3331b0065ec6130bb2f2f44cfb
gisegg <giseggi57@gmail.com>
2022-02-15 00:35:12 +0900

    modified guessgin game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,22 +10,29 @@ fn main() {
 
     println!("The secret number is : {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Falied to read line");
+        let mut guess = String::new();
     
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-
-    println!("Your guess {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin().read_line(&mut guess)
+            .expect("Falied to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+        println!("Your guess {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>  {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
+   
 }
